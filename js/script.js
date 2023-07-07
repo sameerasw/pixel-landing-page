905cf1eedd3630c4f9cdbc0b6b8ea52cf174b0c8
Sameera Sandakelum <68902530+sameerasw@users.noreply.github.com>
2023-07-07 12:27:53 +0530

    new device info page and css,js improvements

--- js/script.js
+++ js/script.js
@@ -4,16 +4,19 @@ let white = document.getElementById('white');
 let black = document.getElementById('black');
 let red = document.getElementById('red');
 let cta = document.getElementsByClassName('cta-primary')[0];
+let ctasecondary = document.getElementsByClassName('cta-secondary')[0];
+let ctaarrow = document.getElementById('cta-arrow');
 
 white.addEventListener('mouseover', () => {
     accent.style.backgroundColor = 'var(--accent-white)';
-    cta.style.backgroundColor = 'var(--accent-black-alt)';
+    cta.style.backgroundColor = 'var(--accent-white)';
     accent2.style.color = 'var(--accent-white-alt)';
-    });
+}
+);
 
 black.addEventListener('mouseover', () => {
     accent.style.backgroundColor = 'var(--accent-black)';
-    cta.style.backgroundColor = 'var(--accent-black)';
+    cta.style.backgroundColor = 'var(--accent-white-alt)';
     accent2.style.color = 'var(--accent-black-alt)';
 }
 );
@@ -22,5 +25,15 @@ red.addEventListener('mouseenter', () => {
     accent.style.backgroundColor = 'var(--accent-red)';
     cta.style.backgroundColor = 'var(--accent-red)';
     accent2.style.color = 'var(--accent-red-alt)';
+}
+);
+
+ctasecondary.addEventListener('mouseover', () => {
+    ctaarrow.style.marginLeft = '10px';
+}
+);
+
+ctasecondary.addEventListener('mouseout', () => {
+    ctaarrow.style.marginLeft = '0px';
 }
 );
